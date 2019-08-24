5f5bfc6b558a7c3e820dca6ad0376323e3d392bc
Ananya <31894391+AnanyaSharmaPy@users.noreply.github.com>
2019-08-24 09:59:41 +0530

    Create election_fill.sol

---
+++ contracts/election_fill.sol
@@ -0,0 +1,40 @@
+pragma solidity ^0.5.0;
+
+contract Election {
+
+    // create a candidate structure
+    struct Candidate {
+
+    }
+    // Store accounts that have voted using a mapping
+
+    // Store Candidates and Fetch Candidate using mapping
+
+    // Store Candidates Count
+
+    // create a event to fire when voted
+
+    //create a constructor to add candidates
+    constructor () public {
+
+    }
+
+    //function to add candidates
+    function addCandidates(string memory _name) public {
+
+    }
+
+    //function to vote
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+
+        // require a valid candidate
+
+        // record that voter has voted
+
+        // update candidate vote Count
+
+        // trigger voted event
+    }
+
+}
